3b1f6a81748cdc09172081598783ae13d8208900
Bhaskar Shah <next181489111@gmail.com>
2025-09-07 16:23:36 +0000

    remove this extra space from each page please literally please

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="relative min-h-screen">
         <AppStateProvider>
-          <main className="pb-24">
+          <main className="pb-28">
             {children}
           </main>
           <GlobalMusicPlayer />
