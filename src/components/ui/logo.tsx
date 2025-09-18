e9f4560ecd0b15dd1cfa196d93663bef14e4c4e7
Bhaskar Shah <next181489111@gmail.com>
2025-09-18 15:24:45 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { cn } from '@/lib/utils';
 
 export const AlmightyLogo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
   <svg
     viewBox="0 0 100 100"
     xmlns="http://www.w3.org/2000/svg"
-    className={cn('h-6 w-6', className)}
+    className={className}
     {...props}
   >
     <defs>
