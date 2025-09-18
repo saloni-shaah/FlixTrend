8c75efb3ddd1f828777199d601d66c4276e2f12a
Bhaskar Shah <next181489111@gmail.com>
2025-09-18 15:28:01 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/almighty/page.tsx
+++ src/app/almighty/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Chat } from "@/components/chat/chat";
+import { Chat } from "../../../almighty/src/components/chat/chat";
 
 export default function AlmightyPage() {
   return (
