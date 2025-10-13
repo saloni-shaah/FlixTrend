a5d70c318d7a7b745680e874b453cf1687413708
Bhaskar Shah <next181489111@gmail.com>
2025-10-13 06:11:02 +0000

    bro i haev added it and it says status couldn't fetch what to do now

---
+++ src/app/sitemap.ts
@@ -0,0 +1,69 @@
+
+import { MetadataRoute } from 'next';
+
+const URL = 'https://flixtrend.in';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${URL}/`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${URL}/guest`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${URL}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${URL}/signup`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${URL}/premium`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+        url: `${URL}/hiring`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.6,
+    },
+    {
+        url: `${URL}/ad-studio`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.6,
+    },
+    {
+        url: `${URL}/privacy`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.3,
+    },
+    {
+        url: `${URL}/terms`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.3,
+    }
+  ];
+}
