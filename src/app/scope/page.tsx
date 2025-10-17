462055950f7865f43ffd491b838d7f2bf69b6bce
Bhaskar Shah <next181489111@gmail.com>
2025-10-17 14:47:22 +0000

    bro are you mad look you have to make the following function of the flix

--- src/app/scope/page.tsx
+++ src/app/scope/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React, { useState, useEffect, useCallback } from "react";
 import { getFirestore, collection, query, where, orderBy, onSnapshot, limit, getDocs } from "firebase/firestore";
