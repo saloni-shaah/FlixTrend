e723a74da3f48df5f3c6f7575415e2b89200eb3c
Bhaskar Shah <next181489111@gmail.com>
2026-01-09 15:01:16 +0000

    Bro, I’m going to be very honest with you — and this is said as someone

--- .next/dev/static/chunks/src_app_page_tsx_4f198bcd._.js
+++ .next/dev/static/chunks/src_app_page_tsx_4f198bcd._.js
@@ -2,10 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_ca88712a._.js",
-  "static/chunks/node_modules_react-icons_fa6_index_mjs_79da82bd._.js",
-  "static/chunks/node_modules_react-icons_lib_844c6c50._.js",
-  "static/chunks/node_modules_dcbdaa34._.js",
-  "static/chunks/node_modules_@fontsource_cinzel_index_2df5e312.css"
+  "static/chunks/node_modules_cc243cd7._.js",
+  "static/chunks/node_modules_@fontsource_08bd7aac._.css"
 ],
     source: "dynamic"
 });
