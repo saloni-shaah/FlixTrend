31c3177b1cda392030abeaf77ecd2a45edb39897
Bhaskar <next181489111@gmail.com>
2026-01-16 18:36:16 +0000

    when i went to save a drop i saw this ## Error Type Console FirebaseErro

---
+++ .next/dev/static/chunks/src_app_create_drop_page_tsx_f033a2fb._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_e99e9299._.js"
+],
+    source: "dynamic"
+});
