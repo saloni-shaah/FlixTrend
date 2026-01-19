2f221ef206e7eaff326cd8d998acd799f46113be
Bhaskar <next181489111@gmail.com>
2026-01-19 16:55:45 +0000

    added drop feature and fix the vibespace and squad page

---
+++ .next/dev/static/chunks/src_app_signal_page_tsx_54f97276._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_6e9eb30b._.js"
+],
+    source: "dynamic"
+});
