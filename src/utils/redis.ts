c9d4539e290c1cce5b9e8c82992209498bd2b2ab
Bhaskar <next181489111@gmail.com>
2026-02-04 06:56:50 +0000

    feat: Use environment variables for Redis configuration

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -2,6 +2,6 @@
 import { Redis } from '@upstash/redis'
 
 export const redisClient = new Redis({
-  url: 'https://calm-unicorn-20569.upstash.io',
-  token: 'AVBZAAIncDI5ZTAwZDQ5NmU3MzU0NGYyYWEyM2MwMWRlNDU3ODI0NnAyMjA1Njk',
+  url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_URL,
+  token: process.env.NEXT_PUBLIC_UPSTASH_REDIS_TOKEN,
 });
