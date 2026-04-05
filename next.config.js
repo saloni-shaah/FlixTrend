9332ec436c5102527bb5ce2c6fdbd998be31adef
Soni Devi <flixtreend@gmail.com>
2026-04-05 04:42:23 +0000

    fix: Correctly handle disabled PWA in next.config.mjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  allowedDevOrigins: ['3000-firebase-studio-1771359278488.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev'],
-};
-
-module.exports = nextConfig;
